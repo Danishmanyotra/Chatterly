4fde5bfe53f2f0732a46df81d9d0951b0dfa3164
Danishmanyotra <danishmanyotra@gmail.com>
2025-11-16 12:56:30 +0530

    change the url in axios and socket file

--- client/src/lib/socket.js
+++ client/src/lib/socket.js
@@ -4,7 +4,7 @@ let socket =null;
 export const connectSocket = (userId) => {
     
     socket = io(
-        import.meta.env.MODE === "development"? "http://localhost:4000": import.meta.env.VITE_API_URL,
+        import.meta.env.MODE === "development"? "http://localhost:4000": "https://chatterly-rbjx.onrender.com",
     {
         query:{userId},
     }    );
