9d49bca30c560fff2d9f001fe06bf66623f94d82
Danishmanyotra <danishmanyotra@gmail.com>
2025-11-16 12:21:57 +0530

    fix: added VITE_API_URL and updated axios/socket

--- client/src/lib/socket.js
+++ client/src/lib/socket.js
@@ -2,8 +2,9 @@ import {io} from 'socket.io-client';
 
 let socket =null;
 export const connectSocket = (userId) => {
+    
     socket = io(
-        import.meta.env.MODE === "development"? "http://localhost:4000": "/",
+        import.meta.env.MODE === "development"? "http://localhost:4000": import.meta.env.VITE_API_URL,
     {
         query:{userId},
     }    );
